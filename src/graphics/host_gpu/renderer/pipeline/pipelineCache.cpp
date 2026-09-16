f236b94ad9630cd8a05d7b400d7133ed370ba81a
MILLYONYT <butterdude420@gmail.com>
2026-09-16 13:47:58 -0600

    Enable Vulkan pipeline cache on Build 32 dirty builds

--- src/graphics/host_gpu/renderer/pipeline/pipelineCache.cpp
+++ src/graphics/host_gpu/renderer/pipeline/pipelineCache.cpp
@@ -486,11 +486,6 @@ void PipelineCache::InitializeDriverCache() {
 		PipelineCacheLog("Vulkan pipeline cache: disabled (unknown git revision)");
 		return;
 	}
-	if (git_hash.ends_with("-dirty")) {
-		PipelineCacheLog("Vulkan pipeline cache: disabled (dirty build)");
-		return;
-	}
-
 	m_driver_cache_path     = std::filesystem::path("_PipelineCache") / (title_id + ".bin");
 	const auto path         = Common::PathToString(m_driver_cache_path);
 	const bool cache_exists = Common::File::IsFileExisting(m_driver_cache_path);
